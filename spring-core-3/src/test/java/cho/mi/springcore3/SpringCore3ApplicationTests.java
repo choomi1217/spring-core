e0518d66af4c3394704e645bf75f9f1ff88ac4a5
youngmi <youngmi.cho@nflux.co.kr>
2024-02-01 10:27:45 +0900

    rename: 1~5까지만 core 1

---
+++ spring-core-3/src/test/java/cho/mi/springcore3/SpringCore3ApplicationTests.java
@@ -0,0 +1,13 @@
+package cho.mi.springcore3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCore3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
