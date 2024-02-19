e7c1cb6097ef75de5d18538112636d0e533b1ae6
choomi1217 <whdudal1217@naver.com>
2024-02-20 00:43:45 +0900

    feat: 국제화

---
+++ spring-core-5/src/main/java/com/example/springcore5/itemservice/TestDataInit.java
@@ -0,0 +1,24 @@
+package com.example.springcore5.itemservice;
+
+import com.example.springcore5.itemservice.domain.item.Item;
+import com.example.springcore5.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
